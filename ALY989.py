55b0521ee33965bb610487d355d5656678d14058
ALY989 <97706596+ALY989@users.noreply.github.com>
2022-01-13 18:28:15 -0500

    Added the start of program

---
+++ ALY989.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env pybricks-micropython
+from pybricks.hubs import EV3Brick
+from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
+                                 InfraredSensor, UltrasonicSensor, GyroSensor)
+from pybricks.parameters import Port, Stop, Direction, Button, Color
+from pybricks.tools import wait, StopWatch, DataLog
+from pybricks.robotics import DriveBase
+from pybricks.media.ev3dev import SoundFile, ImageFile
+
+import challengeTemplate
+
+# Use the following links to access the documentation
+# How to control the EV3: https://docs.pybricks.com/en/v2.0/hubs.html#pybricks.hubs.EV3Brick
+# How to control the robot: https://docs.pybricks.com/en/v2.0/ev3devices.html
+# How to implement timing and logging in your program: https://pybricks.com/ev3-micropython/tools.html
+# Rule book: https://firstinspiresst01.blob.core.windows.net/first-forward/fll-challenge/fll-challenge-cargo-connect-robot-game-rulebook.pdf
+
+# --- Begin Example Program ---
+
+# Initialize the EV3 Brick.
+ev3 = EV3Brick()
+
+# Initialize the motors.
+left_motor = Motor(Port.B)
+right_motor = Motor(Port.C)
+
+# Initialize the drive base. 
+robot = DriveBase(left_motor, right_motor, wheel_diameter=55.5, axle_track=104)
+
+if Button.left == true:
+    while UltrasonicSensor.distance > 20:
+        robot.straight(10)
+if Button.right == true:
+    while UltrasonicSensor.distance > 20:
+        robot.straight(10)
+        robot.turn(90)
+        robot.straight(360)
+        robot.turn(-90)
